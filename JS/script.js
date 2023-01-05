8fbeef732950a9c32f70d8bbf3075773de5d4045
dfsfssdfsdfsd <121974341+dfsfssdfsdfsd@users.noreply.github.com>
2023-01-05 02:27:06 +0100

    Send of the files

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+// Vive Monsieur Hachem , cette partie sert pour l'animation de transition du menue
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
